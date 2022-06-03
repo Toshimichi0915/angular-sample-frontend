ad6bef71ffa35936c1821d61424fc16c02ba8419
Toshimichi0915 <main@toshimichi.net>
2022-06-03 09:52:33 +0900

    Add backend proxy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,30 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'frontend';
+
+  customer = {
+    email: null,
+    username: null
+  }
+
+  async onSubmit() {
+    if (this.customer.email == null) return
+    if (this.customer.username == null) return
+
+    const response = await fetch("/api/customers/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(this.customer)
+    })
+
+    if(!response.ok) {
+      console.error(`Response Error: ${response.status} ${response.statusText}`)
+    }
+  }
 }
